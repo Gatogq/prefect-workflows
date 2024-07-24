73091273571a2e1b4f2e5e53a7ab494c3983bae3
Gatogq <speedyscomin@ciencias.unam.mx>
2024-07-24 13:04:07 -0600

    adding comments to the task and flows of business central project for better clarity

--- projects/Business Central/flows.py
+++ projects/Business Central/flows.py
@@ -11,7 +11,7 @@ from src.sql_engine import SQLServerEngine
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
-project_dir = os.abspath(os.join(os.getcwd(), './'))
+project_dir = os.abspath(os.path.join(os.getcwd(), './'))
 
 sys.path.insert(0, project_dir)
 
@@ -20,6 +20,8 @@ sys.path.insert(0, project_dir)
 
 def update_bc_prod_db(tenant_id,environment,company,client_id,client_secret,server,database):
 
+    #setting an instance of BusinessCentralAPIClient for making the api calls.
+
     APIClient = BusinessCentralAPIClient(tenant_id,
                          environment,
                          company,
@@ -27,15 +29,22 @@ def update_bc_prod_db(tenant_id,environment,company,client_id,client_secret,serv
                          client_secret,
                          )
 
+    #setting an instance of SQLServerEngine for performing SQL operations.
+
     Session = SQLServerEngine(server=server,
                              database=database)
     
     
+    #reading db_schema json which specifies configurations for each sql table
+
     json_path = os.path.join(current_dir,'db_schema.json')
 
     with open(json_path,'r') as file:
 
         db_schema = json.load(file)
+
+    #for each sql table defined on the json schema, obtain its corresponding endpoint, primary key, fields configuration and allowed operations
+    #to perform the update.
 
     for table, table_config in db_schema['tables'].items():
         
@@ -47,7 +56,11 @@ def update_bc_prod_db(tenant_id,environment,company,client_id,client_secret,serv
 
         allowed_ops = table_config['allowed_operations']
 
+        #get new and/or modified and/or deleted records from the endpoint depending on the allowed operations for the specific table.
+
         data = get_records.submit(Session,APIClient,endpoint,table,fields_attr,allowed_ops)
+
+        #update SQL table with three possible operations: insert, update and delete.
 
         update_table_with_dicts.submit(Session,data,table,primary_key,wait_for=[data])
 
--- projects/Business Central/tasks.py
+++ projects/Business Central/tasks.py
@@ -5,7 +5,7 @@ from prefect import task
 from prefect.artifacts import create_markdown_artifact
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-project_dir = os.abspath(os.join(os.getcwd(), './'))
+project_dir = os.abspath(os.path.join(os.getcwd(), './'))
 
 sys.path.insert(0, project_dir)
 
@@ -17,29 +17,46 @@ from src.sql_engine import SQLServerEngine
 def get_records(Session : SQLServerEngine,APIClient : BusinessCentralAPIClient,endpoint : str,table : str,
                 fields : dict,allowed_operations : dict) -> dict:
     
+    #obtain last created record datetime and last modified record datetime from sql table, formated as ISO 8601 standard
+    
     last_created_dt = Session.get_last_update(table,'systemCreatedAt').strftime('%Y-%m-%dT%H:%M:%S.%f')+'Z'
 
     last_modified_dt = Session.get_last_update(table,'systemModifiedAt').strftime('%Y-%m-%dT%H:%M:%S.%f')+'Z'
+
+    #fields to retrieve from the api call, as a list
 
     columns = [_['endpoint_field'] for _ in fields.values()]
 
     inserted_records = [] 
     
     modified_records = []
-    
+
+    #make api calls using the following query parameters: $select to retrieve only required columns and 
+    # $filter to fetch only records created and modified after last database update.
+
     if allowed_operations['insert']:
 
         data =  APIClient.get_with_odata_parameters(api_page=endpoint,createdAt=last_created_dt,select=columns)
+
+        #set column names and apply custom null logic as specified on db_schema.json
 
         inserted_records = [transform_record(_, fields) for _ in data]
 
     if allowed_operations['update']:
 
+
         data =  APIClient.get_with_odata_parameters(api_page=endpoint,createdAt=last_modified_dt,select=columns)
+
+        #set column names and apply custom null logic as specified on db_schema.json
 
         modified_records = [transform_record(_, fields) for _ in data]
 
+        #filter out modified records that are also new records
+
         modified_records = [_ for _ in modified_records if _ not in inserted_records]
+
+
+        #return a dictionary containing the list of dictionaries to insert and to update on sql table.
 
     result = {'insert' : inserted_records, 'update' : modified_records}
 
@@ -47,8 +64,10 @@ def get_records(Session : SQLServerEngine,APIClient : BusinessCentralAPIClient,e
 
 
 @task(name='actualizar tabla SQL',log_prints=True)
-def update_table_with_dicts(SQLSession : SQLServerEngine,dicts : dict,table : str,primary_key : str):
+def update_table_with_dicts(SQLSession : SQLServerEngine,dicts : dict,table : str,primary_key : list):
 
+    #only if 'insert' key contains a non empty list, perform bulk insertion of new records.
+ 
     if 'insert' in dicts:
 
         rows = dicts['insert']
@@ -60,13 +79,18 @@ def update_table_with_dicts(SQLSession : SQLServerEngine,dicts : dict,table : st
             markdown_table = DataFrame(rows).to_markdown(index=True)
 
             description = f'{len(rows)} registros nuevos'
+
+            #create a markdown table of the inserted rows to show on the prefect UI
             
             create_markdown_artifact(markdown=markdown_table,description=description)
 
         else:
 
             print(f'No se encontraron registros nuevos para añadir a la tabla {table}')
-        
+
+    
+    #only if 'update' key contains a non empty list, perform update of modified records based on its primary key.
+       
     if 'update' in dicts:
 
         rows = dicts['update']
@@ -79,6 +103,8 @@ def update_table_with_dicts(SQLSession : SQLServerEngine,dicts : dict,table : st
 
             description = f'{len(rows)} registros actualizados'
 
+            #create a markdown table of the modified rows to show on the prefect UI
+
             create_markdown_artifact(markdown=markdown_table,description=description)
 
         else:
@@ -89,7 +115,12 @@ def update_table_with_dicts(SQLSession : SQLServerEngine,dicts : dict,table : st
 
 def transform_record(row : dict,fields : dict):
 
+    #for each dictionary which represents a row on the api response, rename, mapping the api fields to the sql table fields.
+
     column_map = {v['endpoint_field']: k for k, v in fields.items()}
+
+    #custom mapping dictionaries, for certain columns as specified on db_schema.json, mapping empty values to None
+    # which is compatible with SQL NULL value
 
     str_null_map = {'' : None,' ' : None}
 
@@ -98,6 +129,8 @@ def transform_record(row : dict,fields : dict):
     date_null_map = { '0001-01-01' : None}
 
     row = {column_map.get(k, k): v for k, v in row.items()}
+
+    #custom logic that iterates over dictionary values to set None for empty values.
 
     for f, attr in fields.items():
 
